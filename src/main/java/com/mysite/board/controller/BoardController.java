412d73aadec8be16225e7cad0bd932d2596630bb
Youngwook Choi <duddnr3618@gmail.com>
2023-07-31 12:10:49 +0900

    2차 : 게시글 리스트 클릭스 뷰페이지로 이동

--- src/main/java/com/mysite/board/controller/BoardController.java
+++ src/main/java/com/mysite/board/controller/BoardController.java
@@ -9,32 +9,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.mysite.board.entity.Board;
 import com.mysite.board.service.BoardService;
 
+import jakarta.persistence.Id;
+
 @Controller
 public class BoardController {
 	
 	@Autowired
 	private BoardService boardService;
 
+	//게시글 작성 폼
 	@GetMapping("/board/write")	//localhost:8002/board/write
 	public String boardWriteForm () {
 		
 		return "boardwrite";
 	}
 	
+	//게시글 작서후 db에 전송 폼
 	@PostMapping("/board/writepro")
 	public String boardWritePro (Board board) {
 
-		boardService.wirte(board);
+	boardService.wirte(board);
+
 		
 		return "";
 	}
 	
+	//게시물의 리스트에 불러오는 폼
 	@GetMapping("/board/list")
 	public String boardList (Model model) {
 		
-		model.addAttribute("list",boardService.boardList());
+		model.addAttribute ("list", boardService.boardList());
 		return "boardlist";
 	}
 	
-	
+	//상세페이지
+	@GetMapping("/board/view")
+	public String boaordView(Model model , Integer id) {
+		
+		model.addAttribute("board" , boardService.boardView(id));
+		return "boardview";
+	}
+
 }
